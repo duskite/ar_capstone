7e26bc63f674caf202c8e7f373e9fd144057ce2e
duskite <duskite@github.com>
2022-03-01 16:30:51 +0900

    커밋 제대로 안돼서 다시

--- app/src/main/java/com/mju/ar_capstone/helpers/FirebaseManager.java
+++ app/src/main/java/com/mju/ar_capstone/helpers/FirebaseManager.java
@@ -1,4 +1,51 @@
 package com.mju.ar_capstone.helpers;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class FirebaseManager {
+
+    private DatabaseReference mDatabase;
+    private static final String DB_REGION = "https://ar-capstone-dbf8e-default-rtdb.asia-southeast1.firebasedatabase.app";
+
+    //이 부분은 나중에 좀 변경해야할듯 db구조에 맞게
+    private static final String KEY_ROOT_DIR = "firebase_manager";
+
+    //게시글 아이디 임시로
+    private static int cnt = 0;
+
+    public FirebaseManager(){
+        mDatabase = FirebaseDatabase.getInstance(DB_REGION).getReference().child(KEY_ROOT_DIR);
+        DatabaseReference.goOnline();
+    }
+
+    public void putText(String string){
+        mDatabase.child(String.valueOf(cnt)).setValue(string);
+        cnt++;
+    }
+
+    public void getText(){
+
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Log.d("L_getText", snapshot.getValue().toString());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+    }
 }
