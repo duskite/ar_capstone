3fe49b2d1e23483c99bc32c59d2fe806dbcaa02d
duskite <duskite@github.com>
2022-03-17 13:40:22 +0900

    firebase 익명 로그인 구현

--- app/src/main/java/com/mju/ar_capstone/MainActivity.java
+++ app/src/main/java/com/mju/ar_capstone/MainActivity.java
@@ -11,7 +11,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -21,6 +27,7 @@ import com.mju.ar_capstone.helpers.FirebaseManager;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseAuth firebaseAuth;
     private FirebaseManager firebaseManager;
     private Button btnMap;
     //화면 요소들 선언
@@ -35,6 +42,9 @@ public class MainActivity extends AppCompatActivity {
         // 씬 폼 데트스용
         btnArSf = (Button) findViewById(R.id.btnArSf);
 
+        //익명 로그인 구현중
+        firebaseAuth = FirebaseAuth.getInstance();
+        singInAnonymously();
 
         //아래 기존 코드를 firebase manager로 대체중
         firebaseManager = new FirebaseManager();
@@ -60,4 +70,29 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    //익명 로그인 처리
+    private void singInAnonymously(){
+        // [START signin_anonymously]
+        firebaseAuth.signInAnonymously()
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("순서", "signInAnonymously:success");
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            Log.d("순서", user.toString());
+                            Log.d("순서", user.getUid().toString());
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("순서", "signInAnonymously:failure", task.getException());
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+        // [END signin_anonymously]
+    }
+
 }
