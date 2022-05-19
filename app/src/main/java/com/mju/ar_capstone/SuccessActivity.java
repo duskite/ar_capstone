46d92b71c9f549b747718be4629171e615a292d2
duskite <duskite@github.com>
2022-05-20 01:56:37 +0900

    konfetti로 게임 클리어시 효과 적용

--- app/src/main/java/com/mju/ar_capstone/SuccessActivity.java
+++ app/src/main/java/com/mju/ar_capstone/SuccessActivity.java
@@ -3,9 +3,15 @@ package com.mju.ar_capstone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 import com.mju.ar_capstone.helpers.FirebaseManager;
+
+import nl.dionsegijn.konfetti.KonfettiView;
+import nl.dionsegijn.konfetti.models.Shape;
+import nl.dionsegijn.konfetti.models.Size;
 
 public class SuccessActivity extends AppCompatActivity {
 
@@ -13,6 +19,18 @@ public class SuccessActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_success);
+
+        final KonfettiView konfettiView = findViewById(R.id.konfettiView);
+        konfettiView.build()
+                .addColors(Color.YELLOW, Color.GREEN, Color.MAGENTA)
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(Shape.Square.INSTANCE, Shape.Circle.INSTANCE)
+                .addSizes(new Size(12, 5f))
+                .setPosition(-50f, konfettiView.getWidth() + 50f, -50f, -50f)
+                .streamFor(300, 5000L);
 
     }
 }
