1f5a812adcac1d00d04e6d870a6bcca1ef2aecb8
duskite <duskite@github.com>
2022-05-20 18:31:14 +0900

    알림 기능 개선 - 그룹화, pendingIntend, 기존에 우승자 알림 안오던거 수정

--- app/src/main/java/com/mju/ar_capstone/services/NotificationService.java
+++ app/src/main/java/com/mju/ar_capstone/services/NotificationService.java
@@ -3,6 +3,7 @@ package com.mju.ar_capstone.services;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -20,11 +21,14 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.mju.ar_capstone.MainActivity;
 import com.mju.ar_capstone.R;
 import com.mju.ar_capstone.helpers.FirebaseManager;
+import com.mju.ar_capstone.managefragments.WinnerListFragment;
 
 public class NotificationService extends Service {
 
+    private static String NOTI_GROUP = "neAR";
 
     private DatabaseReference mReference;
     private ChildEventListener mChild;
@@ -49,14 +53,24 @@ public class NotificationService extends Service {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
 
             notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            notificationChannel = notificationManager.getNotificationChannel("winner");
-            if(notificationChannel == null){
-                notificationChannel = new NotificationChannel("winner", "winner", NotificationManager.IMPORTANCE_HIGH);
-                notificationManager.createNotificationChannel(notificationChannel);
-            }
+            notificationChannel = new NotificationChannel("winner", "우승자 알림", NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.setDescription("우승자 발생시 알림을 받음");
+            notificationManager.createNotificationChannel(notificationChannel);
 
-            Notification notification = new NotificationCompat.Builder(this, "winner").build();
-            startForeground(1,notification);
+            Intent intent = new Intent(this, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,0);
+
+
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "winner");
+            builder.setSmallIcon(R.drawable.play_icon);
+            builder.setContentTitle("neAR");
+            builder.setContentText("우승자 알림을 받을 수 있습니다.");
+            builder.setContentIntent(pendingIntent);
+//            Notification notification = new NotificationCompat.Builder(this, "winner")
+//                    .setContentTitle("neAR")
+//                    .setContentText("게임을 클리어한 유저가 생길 경우 알림을 받게 됩니다.")
+//                    .build();
+            startForeground(1, builder.build());
         }
 
     }
@@ -77,48 +91,39 @@ public class NotificationService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-//        Log.d("알림", "onBind");
-//
-//        channel = intent.getStringExtra("selectedChannel");
-//        Log.d("알림", channel);
         // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
-
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        try{
+        Log.d("알림", "onStartCommand");
 
-        }catch (NullPointerException e){
-            Log.d("알림", "onStartCommand");
+        try{
             selectedChannel = intent.getStringExtra("selectedChannel");
             Log.d("알림", selectedChannel);
             initDB(selectedChannel);
+        }catch (NullPointerException e){
+            Log.d("알림", "onStartCommand 인텐트 null");
         }
 
         return super.onStartCommand(intent, flags, startId);
     }
     public void sendNotification(String user){
         //알림띄우기
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "default");
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "winner");
 
         builder.setSmallIcon(R.drawable.play_icon);
         builder.setContentTitle("방금 게임을 클리어한 유저가 있습니다.");
-        builder.setContentText("ID: " + user);
+        builder.setContentText("채널: " + selectedChannel + "         ID: " + user);
 
         builder.setColor(Color.GREEN);
+
         builder.setAutoCancel(true);
+        builder.setGroup(NOTI_GROUP);
 
-        //알림클릭시 액션 인텐트
-//        builder.setContentIntent(pendingIntent);
-
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
-            notificationManager.createNotificationChannel(new NotificationChannel("default","중요",NotificationManager.IMPORTANCE_HIGH));
-        }
-
-        notificationManager.notify(1,builder.build());
+        notificationManager.notify((int)(System.currentTimeMillis()/1000), builder.build());
 
     }
 
@@ -138,7 +143,6 @@ public class NotificationService extends Service {
                     }catch (NullPointerException e){
 
                     }
-
                 }
 
                 @Override
